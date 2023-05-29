34db9bda3b3cec3f3861e32c626fca2ad98c8459
enzonjagi <amosndungo@gmail.com>
2023-05-29 16:52:54 +0300

    Calendar view with an App bar

--- App_two/flutter_event_cal/lib/main.dart
+++ App_two/flutter_event_cal/lib/main.dart
@@ -28,7 +28,87 @@ class _CalendarPageState extends State<CalendarPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SfCalendar(),
+      appBar: AppBar(
+        backgroundColor: Colors.brown,
+        title: Text('The Calendar'),
+      ),
+      body: SfCalendar(
+        view: CalendarView.week, //TODO Allow users to select this
+        firstDayOfWeek: 1,
+        initialDisplayDate: DateTime.now(),
+        dataSource: MeetingDataSource(_getDataSource()),
+        monthViewSettings: const MonthViewSettings(
+          appointmentDisplayMode: MonthAppointmentDisplayMode.appointment,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+        // add here
+        },
+        child: Text('+', textAlign: TextAlign.center,),
+        backgroundColor: Colors.brown,
+      ),
     );
   }
+
+  List<Meeting> _getDataSource() {
+    final List<Meeting> meetings = <Meeting>[];
+    final DateTime today = DateTime.now();
+    final DateTime startTime = DateTime(
+      today.year,
+      today.month,
+      today.day,
+      9,0,0
+    );
+    final DateTime endtime = startTime.add(
+      const Duration(hours: 2)
+    );
+
+    meetings.add(Meeting(
+      'Conference', startTime, endtime, const Color(0xFF0F8644), false)
+    );
+
+    return meetings;
+  }
+}
+
+class MeetingDataSource extends CalendarDataSource {
+  MeetingDataSource(List<Meeting> source) {
+    appointments = source;
+  }
+
+  @override
+  DateTime getStartTime(int index) {
+    return appointments![index].from;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    return appointments![index].to;
+  }
+
+  @override
+  String getSubject(int index) {
+    return appointments![index].eventName;
+  }
+
+  @override
+  Color getColor(int index) {
+    return appointments![index].background;
+  }
+
+  @override
+  bool isAllDay(int index) {
+    return appointments![index].isAllDay;
+  }
+}
+
+class Meeting {
+  Meeting(this.eventName, this.from, this.to, this.background, this.isAllDay);
+
+  String eventName;
+  DateTime from;
+  DateTime to;
+  Color background;
+  bool isAllDay;
 }
